7da8488169b94b650e99aed0d3fe549cf63dd266
勾洋洋 <gouyy@fiscan.cn>
2021-08-06 15:52:35 +0800

    初始版本

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    title: '顽石',
+    description: '顽石个人博客',
+    head: [
+        ['link',
+            { rel: 'icon', href: '/imgs/miao.jpg' }
+        ]
+    ],
+    themeConfig: {
+        logo: '/imgs/miao.jpg',
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '分类',
+                ariaLabel: '分类',
+                items: [
+                    { text: '文章', link: '/pages/article/article1.md' },
+                    { text: '言语', link: '/pages/something/something1.md' }
+                ]
+            }
+        ],
+        sidebar: {
+            '/pages/article/': [{
+                title: '文章菜单1',
+                // collapsable: false,
+                // sidebarDepth: 1,
+                children: [
+                    ['article1.md', '文章1'],
+                    ['article2.md', '文章2']
+                ]
+            }, ],
+            '/pages/something/': [{
+                title: '文章菜单2',
+                collapsable: false,
+                sidebarDepth: 1,
+                children: [
+                    ['/pages/something/something1.md', '文章1'],
+                    ['/pages/something/something2.md', '文章2']
+
+                ]
+            }, ]
+        }
+    },
+
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /imgs/miao.jpg
+heroText: 顽石
+tagline: 点滴积累
+actionText: GO！GO! GO!
+actionLink: /pages/test.md
+features:
+- title: 厚积薄发
+  details: 不积硅步无以至千里。
+- title: 坚持
+  details: 坚持就已经是你的胜利
+
+---
