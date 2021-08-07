0b29beb8699d4c583ed0839218d1fa906be194da
勾洋洋 <gouyy@fiscan.cn>
2021-08-07 15:31:51 +0800

    修改首页

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 打包生成静态文件
+npm run build
+
+# 进入打包好的文件夹
+cd docs/.vuepress/dist
+
+# 创建git的本地仓库，提交修改
+git init
+git add -A
+git commit -m 'deploy'
+
+# 覆盖式的将本地仓库发布至github
+git push -f  git@github.com:GYY2046/GYY2046.github.io.git master
+
+cd -
--- docs/README.md
+++ docs/README.md
@@ -7,7 +7,9 @@ actionText: GO！GO! GO!
 actionLink: /pages/test.md
 features:
 - title: 厚积薄发
-  details: 不积硅步无以至千里。
+  details: 不积硅步无以至千里
+- title: 积重难返
+  details: 各种小问题积累到一定程度就会导致整个系统的崩溃，无法挽回
 - title: 坚持
   details: 坚持就已经是你的胜利
 
