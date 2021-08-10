169b88058e6de3331f2e89cabda0406d4e1c22df
gyy2046 <gyy2046@outlook.com>
2021-08-10 10:14:06 +0800

    调整忽略文件

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ cd docs/.vuepress/dist
 # 创建git的本地仓库，提交修改
 git init
 git add -A
+git config user.email 'gyy2046@outlook.com'
+git config user.name 'gyy2046'
 git commit -m 'deploy'
 
 # 覆盖式的将本地仓库发布至github
